7dafed06863302db724c1345b2b69857475a400d
jun127 <kimsj012731@gmail.com>
2024-03-28 01:27:38 +0900

    Modifty error for vercel

--- src/app/(afterLogin)/_component/LogoutButton.tsx
+++ src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -1,25 +1,46 @@
 "use client";
 
 import style from "./logoutButton.module.css";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { Session } from "@auth/core/types";
+import { useQueryClient } from "@tanstack/react-query";
 
-export default function LogoutButton() {
-  const me = {
-    // 임시로 내 정보 있는것처럼
-    id: "jun0127",
-    nickname: "김성준",
-    image: "/5Udwvqim.jpg",
+type Props = {
+  me: Session | null;
+};
+export default function LogoutButton({ me }: Props) {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const onLogout = () => {
+    queryClient.invalidateQueries({
+      queryKey: ["posts"],
+    });
+    queryClient.invalidateQueries({
+      queryKey: ["users"],
+    });
+    signOut({ redirect: false }).then(() => {
+      fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/logout`, {
+        method: "post",
+        credentials: "include",
+      });
+      router.replace("/");
+    });
   };
 
-  const onLogout = () => {};
+  if (!me?.user) {
+    return null;
+  }
 
   return (
     <button className={style.logOutButton} onClick={onLogout}>
       <div className={style.logOutUserImage}>
-        <img src={me.image} alt={me.id} />
+        <img src={me.user?.image as string} alt={me.user?.email as string} />
       </div>
       <div className={style.logOutUserName}>
-        <div>{me.nickname}</div>
-        <div>@{me.id}</div>
+        <div>{me.user?.name}</div>
+        <div>@{me.user?.email}</div>
       </div>
     </button>
   );
